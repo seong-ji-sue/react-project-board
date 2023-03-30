ebda43e35baaa3b6755cd1ff11dac4e78698eaf8
jisue <tjdwltn121@gmail.com>
2023-03-30 19:35:34 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import BulletinBoard from "./noticeBoard/BulletinBoard";
+// import NoticeBoard from "./noticeBoard/NoticeBoard";
+
+function App() {
+  return (
+    <div className="App">
+			{/*<BulletinBoard />*/}
+			{/*<NoticeBoard />*/}
+    </div>
+  );
+}
+
+export default App;
