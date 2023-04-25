ed10c888ab5b85430ecefcd58b071ee1f8eb9e2e
jisue <tjdwltn121@gmail.com>
2023-04-26 03:14:30 +0900

    action 타입 지정과 글 작성 완료 시 이벤트 수정

---
+++ src/type/context.ts
@@ -0,0 +1,10 @@
+
+export enum ActionType {
+	init =  'FETCH_INIT',
+	success = 'FETCH_SUCCESS' ,
+	failure = 'FETCH_FAILURE' ,
+	add = 'ADD_POST' ,
+	update = 'UPDATE_POST' ,
+	delete = 'DELETE_POST' ,
+}
+
